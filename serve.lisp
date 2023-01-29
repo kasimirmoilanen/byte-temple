7f1c945f09e40f61b54a250d889a265714c9ad41
kasimirmoilanen <46940191+kasimirmoilanen@users.noreply.github.com>
2023-01-29 02:18:01 +0200

    Create serve.lisp

---
+++ serve.lisp
@@ -0,0 +1,30 @@
+(ql:quickload :hunchentoot)
+(ql:quickload :spinneret)
+
+; On a unix system, the root seems to be the user's home directory.
+(defparameter *path* "./path/to/file-directory/")
+
+(defparameter *server* (make-instance 'hunchentoot:easy-acceptor
+                                      :port 33333
+                                      :address "0.0.0.0"
+                                      :document-root (truename *path*)))
+
+
+
+(defun start ()
+  (hunchentoot:start *server*))
+
+(defun stop ()
+  (hunchentoot:stop *server*))
+
+(hunchentoot:define-easy-handler (main :uri "/")
+    ()
+  (spinneret:with-html-string
+    (:html
+     (:head)
+     (:body (:h1 "Files")
+            (:h4 "Made with secret alien technology...")
+            (loop :for i :in (uiop:directory-files *path*) :collect
+                  (:p (:a :href (file-namestring i) (file-namestring i))))
+            (:br)
+            ))))
